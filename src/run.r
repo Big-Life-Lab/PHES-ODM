241c2a53a18c0eb3093699c7cd71157efb2cebda
hswerdfe <howard.swerdfeger@canada.ca>
2020-12-23 12:53:40 -0500

    added mapping files for three major contributors of data.

--- src/run.r
+++ src/run.r
@@ -6,16 +6,16 @@ source("src/wbe_db_func.r")
 #wbe_delete_whole_db()
 
 
-map_from <- "UW"
-
-
 mappers <- wbe_read_mappers()
 
 dfs <- mappers$NML$reader(file.path(mappers$NML$mapdir, mappers$NML$fn))
 
 dfs_UW <- wbe_read_and_map(nm = "UW")
 dfs_NML <- wbe_read_and_map(nm = "NML")
-dfs_O <- wbe_read_and_map(nm = "Ottawa")
+dfs_Ottawa <- wbe_read_and_map(nm = "Ottawa")
 
-dfs_O %>% names()
-dfs_O$Measurement
+
+wbe_append_from_dfs(dfs_UW)
+wbe_append_from_dfs(dfs_NML)
+wbe_append_from_dfs(dfs_Ottawa)
+
