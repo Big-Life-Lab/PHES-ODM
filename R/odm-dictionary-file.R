c01327f6d2c1e1d1035570cd48a320c33bbc3a21
rvyuha <rostikisawesome@gmail.com>
2023-06-23 11:11:49 -0400

    [Feature] Added files validation from PHES-ODM-Doc

---
+++ R/odm-dictionary-file.R
@@ -0,0 +1,9 @@
+odm_dictionary <- list()
+
+odm_dictionary$dictionary_directory <- "data/raw"
+odm_dictionary$tmp_dictionary_directory <- "data/tmp"
+odm_dictionary$dictionary_missing_value_replacement <- "NA"
+odm_dictionary$parts_sheet_name <- "parts"
+odm_dictionary$sets_sheet_name <- "sets"
+odm_dictionary$files_sheet_name <- "files"
+odm_dictionary$summary_sheet_name <- "summary"
